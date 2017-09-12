c90c35de6aa26a66300d43878f6ba0edc086f97e
Aditya Sharma <aditya.sharma23@outlook.com>
2017-09-12 11:08:47 +0530

    Add project files.

---
+++ EmptyStringDummy/Converters/EmptyStringConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace EmptyStringDummy.Converters
+{
+    public class EmptyStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            //if (value is string passedvalue)
+            //{
+            //    if (string.IsNullOrEmpty(passedvalue))
+            //        return "-empty-";
+            //}
+            //return value;
+
+            if (value is string && string.IsNullOrWhiteSpace((string)value))
+            {
+                return "-empty-";
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EmptyStringDummy/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace EmptyStringDummy
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Loaded += (s, e) =>
+            {
+                DataSource = new ObservableCollection<string>(new List<string>()
+                {
+                    "1. Batman",
+                    "2. Spiderman",
+                    "3. Hulk",
+                    "4. Superman",
+                    "",
+                    "6. Wonder Woman"
+
+                });
+
+            };
+
+        }
+
+
+        private ObservableCollection<string> dataSource;
+        public ObservableCollection<string> DataSource
+        {
+            get { return dataSource; }
+            set { dataSource = value; RaisePropertyChanged(nameof(DataSource)); }
+        }
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
